fb55738ee607646420fc008b8e7b55527ac72a9d
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-27 10:12:57 +0900

    update to new pex_config multi behavior

--- config/processCoadd.py
+++ config/processCoadd.py
@@ -42,8 +42,7 @@ root.detection.thresholdType = "pixel_stdev"
 try:
     # Enable multiShapelet for model mags.
     import lsst.meas.extensions.multiShapelet
-    root.measurement.algorithms.names += ("multishapelet.psf", "multishapelet.exp", "multishapelet.dev", 
-                                          "multishapelet.combo")
+    root.measurement.algorithms.names.update(lsst.meas.extensions.multiShapelet.algorithms)
     root.measurement.slots.modelFlux = "multishapelet.combo.flux"
     # PSF should be exactly double-Gaussian (zeroth-order shapelet)
     root.measurement.algorithms["multishapelet.psf"].innerOrder = 0
