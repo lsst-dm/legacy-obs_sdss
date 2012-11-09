14f153f75d48dfad085c7be4fa85655c7004102e
Russell Owen <rowen@uw.edu>
2012-11-09 12:17:40 -0800

    Starter implementation of task for SDSS-specific zeropoint scaling

---
+++ python/lsst/obs/sdss/scaleSdssZeroPoint.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+#
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+import MySQLdb
+import os
+
+import lsst.pex.config as pexConfig
+from lsst.afw.coord import IcrsCoord
+import lsst.afw.geom as afwGeom
+from lsst.daf.persistence import DbAuth
+import lsst.pipe.base as pipeBase
+from lsst.pipe.tasks.selectImages import BaseSelectImagesConfig, BaseExposureInfo
+from coaddUtils import ImageScaler, ScaleZeroPointTask
+
+__all__ = ["ScaleSdssZeroPointTask"]
+
+class SdssImageScaler(object):
+    def __init__(self):
+        """Construct a multiplicative scale factor.
+        
+        It consists of  a list of points in tract coordinates. Each point has an X, Y, and a scalefactor.
+        """
+        self.xList = []
+        self.yList = []
+        self.scaleList = []
+        #self.scaleErrList = []
+
+    def scaleMaskedImage(self, maskedImage):
+        """Apply scale correction to the specified masked image
+        
+        @param[in,out] image to scale; scale is applied in place
+        """
+        scale = self.getInterpImage(maskedImage.getBBox(afwImage.PARENT)
+        maskedImage *= scale
+
+    def getInterpImage(self, bbox):
+        """Return an image interpolated in R.A direction covering supplied bounding box
+        
+         Hard-coded to work with obs_sdss only
+        """
+        npoints = len(self.xList)
+        xvec = afwMath.vectorF(self.xList)
+        zvec = afwMath.vectorF(self.scaleList)      
+        height = bbox.getHeight()
+        width = bbox.getWidth()
+        x0, y0 = bbox.getBegin()
+
+        # I can't get makeInterpolate to work "Message: gsl_interp_init failed: invalid argument supplied by user [4]"
+        # interp = afwMath.makeInterpolate(xvec, zvec, afwMath.Interpolate.LINEAR)
+        # interp = afwMath.makeInterpolate(xvec, zvec) won't initialize spline. 
+        # eval = interp.interpolate(range(y0, height))
+        
+        eval = numpy.interp(range(x0, x0 + width), xvec, zvec)
+        
+        evalGrid = numpy.meshgrid(eval.astype(numpy.float32),range(0, height))[0]
+        image = afwImage.makeImageFromArray(evalGrid)
+        image.setXY0(x0, y0)
+        return image
+
+
+class ScaleSdssZeroPointConfig(ScaleZeroPointTask.ConfigClass):
+    selectFluxMag0 = pexConfig.Configurable(
+        doc = "Task to select data to compute spatially varying photometric zeropoint",
+        dtype = ScaleSdssZeroPointTask,
+    )
+
+
+class ScaleSdssZeroPointTask(ScaleZeroPointTask):
+    """Select SDSS images suitable for coaddition
+    """
+    ConfigClass = ScaleSdssZeroPointConfig
+
+    
+    def getScaleFromDb(self, patchRef, wcs, bbox):
+        """
+        Query a database for fluxMag0s and return a SdssImageScaler
+
+        First, double the width (R.A. direction) of the patch bounding box. Query the database for
+        overlapping fluxMag0s corresponding to the same run and filter.
+        
+        """
+        scaleFactor = SdssImageScaler()
+        buffer = bbox.getWidth()//2
+        biggerBbox = afwGeom.Box2I(afwGeom.Point2I(bbox.getBeginX()-buffer, bbox.getBeginY()),
+                                   afwGeom.Extent2I(bbox.getWidth()+buffer, bbox.getHeight()))
+        cornerPosList = afwGeom.Box2D(biggerBbox).getCorners()
+        coordList = [wcs.pixelToSky(pos) for pos in cornerPosList]
+        runArgDict = self.selectFluxMag0._runArgDictFromDataId(patchRef.dataId)
+        fluxMagInfoList = self.selectFluxMag0.run(
+        
+            coordList, **runArgDict).fluxMagInfoList
+
+        for fluxMagInfo in fluxMagInfoList:
+            print fluxMagInfo.dataId, self.fluxMag0ToScale(fluxMagInfo.fluxMag0)
+            raCenter = (fluxMagInfo.coordList[0].getRa() +  fluxMagInfo.coordList[1].getRa() +
+                        fluxMagInfo.coordList[2].getRa() +  fluxMagInfo.coordList[3].getRa())/ 4.
+            decCenter = (fluxMagInfo.coordList[0].getDec() +  fluxMagInfo.coordList[1].getDec() +
+                        fluxMagInfo.coordList[2].getDec() +  fluxMagInfo.coordList[3].getDec())/ 4.
+            x, y = wcs.skyToPixel(raCenter,decCenter)
+            scaleFactor.xList.append(x)
+            scaleFactor.yList.append(y)          
+            scaleFactor.scaleList.append(self.fluxMag0ToScale(fluxMagInfo.fluxMag0))
+            #self.scaleFactor.scaleErrList.append(self.fluxMag0ToScale(fluxMagInfo.fluxMag0Sigma))
+        return scaleFactor
---
+++ python/lsst/obs/sdss/selectFluxMag0.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+#
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+import MySQLdb
+import os
+
+import lsst.pex.config as pexConfig
+from lsst.afw.coord import IcrsCoord
+import lsst.afw.geom as afwGeom
+from lsst.daf.persistence import DbAuth
+import lsst.pipe.base as pipeBase
+from lsst.pipe.tasks.selectImages import BaseSelectImagesConfig, BaseExposureInfo
+
+__all__ = ["SelectSdssfluxMag0Task"]
+
+class SelectSdssfluxMag0Config(BaseSelectImagesConfig):
+    table = pexConfig.Field(
+        doc = "Name of database table",
+        dtype = str,
+        default = "Science_Ccd_Exposure",
+    )
+
+    def setDefaults(self):
+        BaseSelectImagesConfig.setDefaults(self)
+        self.database = "krughoff_early_prod_11032012"
+
+
+class FluxMagInfo(BaseExposureInfo):
+    """Data about a selected exposure
+    
+    Data includes:
+    - dataId: data ID of exposure (a dict)
+    - coordList: a list of corner coordinates of the exposure (list of IcrsCoord)
+    - fluxMag0: float 
+    - fluxMag0Sigma: float
+    """
+    def __init__(self, result):
+        """Set exposure information based on a query result from a db connection
+        """
+        BaseExposureInfo.__init__(self)
+        self.dataId = dict(
+           run = result[self._nextInd],
+           camcol = result[self._nextInd],
+           field = result[self._nextInd],
+           filter = result[self._nextInd],
+        )
+        self.coordList = []
+        for i in range(4):
+            self.coordList.append(
+                IcrsCoord(
+                    afwGeom.Angle(result[self._nextInd], afwGeom.degrees),
+                    afwGeom.Angle(result[self._nextInd], afwGeom.degrees),
+                )
+            )
+        self.fluxMag0 = result[self._nextInd]
+        self.fluxMag0Sigma = result[self._nextInd]
+                
+    @staticmethod
+    def getColumnNames():
+        """Get database columns to retrieve, in a format useful to the database interface
+        
+        @return database column names as list of strings
+        """
+        return (
+            "run camcol field  filterName".split() + \
+            "corner1Ra corner1Decl corner2Ra corner2Decl corner3Ra corner3Decl corner4Ra corner4Decl".split() + \
+            "fluxMag0 fluxMag0Sigma".split()
+        )
+
+class SelectSdssfluxMag0Task(pipeBase.Task):
+    """Select SDSS data suitable for computing fluxMag0
+    """
+    ConfigClass = SelectSdssfluxMag0Config
+
+    @pipeBase.timeMethod
+    def run(self, filter, run, camcol):
+        """Select flugMag0's of SDSS images for a particular run
+
+        @param[in] filter: filter for images (one of "u", "g", "r", "i" or "z")
+        @param[in] coordList: list of coordinates defining region of interest
+        
+        @return a pipeBase Struct containing:
+        - fluxMagInfoList: a list of FluxMagInfo objects
+        """
+        if filter not in set(("u", "g", "r", "i", "z")):
+            raise RuntimeError("filter=%r is an invalid name" % (filter,))
+
+        read_default_file=os.path.expanduser("~/.my.cnf")
+
+        try:
+            open(read_default_file)
+            kwargs = dict(
+                read_default_file=read_default_file,
+                )
+        except IOError:
+            kwargs = dict(
+                user = DbAuth.username(self.config.host, str(self.config.port)),
+                passwd = DbAuth.password(self.config.host, str(self.config.port)),
+                )
+
+        self.log.info(self.config.table)    
+        self.log.info(self.config.database)
+        
+        db = MySQLdb.connect(
+            host = self.config.host,
+            port = self.config.port,
+            db = self.config.database,
+            **kwargs
+        )
+        cursor = db.cursor()
+        
+        columnNames = tuple(FluxMagInfo.getColumnNames())
+        if not columnNames:
+            raise RuntimeError("Bug: no column names")
+        queryStr = "select %s from %s where " % (", ".join(columnNames), self.config.table)
+        dataTuple = () # tuple(columnNames)
+
+        # compute where clauses as a list of (clause, data)
+        whereDataList = [
+            ("filterName = %s", filter),
+            ("run = %s", run),
+            ("camcol = %s", camcol),
+        ]
+        
+        queryStr += " and ".join(wd[0] for wd in whereDataList)
+        dataTuple += tuple(wd[1] for wd in whereDataList)
+        
+        queryStr += " order by field"
+
+        self.log.info("queryStr=%r; dataTuple=%s" % (queryStr, dataTuple))
+        
+        cursor.execute(queryStr, dataTuple)
+        exposureInfoList = [FluxMagInfo(result) for result in cursor]        
+       
+        self.log.info("Found %d exposures" % \
+            (len(exposureInfoList)))
+        
+        return pipeBase.Struct(
+            fluxMagInfoList = exposureInfoList,
+        )
