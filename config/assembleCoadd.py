6c68232f7742a56809864bc3a741f840ff1c58ae
Russell Owen <rowen@uw.edu>
2012-11-09 13:56:36 -0800

    Retarget zero point scaling task in assembleCoadd

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,4 +1,6 @@
 # overrides for pipe_tasks CoaddTask.ConfigClass
 from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
+from lsst.obs.sdss.scaleSdssZeroPoint import ScaleSdssZeroPoint
 
 root.select.retarget(SelectSdssImagesTask)
+root.scaleZeroPoint.retarget(ScaleSdssZeroPoint)
