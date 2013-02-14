60c958eda0fe6a9a43fd920187dfc4b974646869
Yusra AlSayyad <yusra@astro.washington.edu>
2013-02-13 22:37:49 -0600

    moved flux scaling step from assembleCoadd to makeCoaddTempExp

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,6 +1,5 @@
 # overrides for pipe_tasks CoaddTask.ConfigClass
 from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
-from lsst.obs.sdss.scaleSdssZeroPoint import ScaleSdssZeroPointTask
 
 root.select.retarget(SelectSdssImagesTask)
-root.scaleZeroPoint.retarget(ScaleSdssZeroPointTask)
+
--- config/makeCoaddTempExp.py
+++ config/makeCoaddTempExp.py
@@ -1,4 +1,6 @@
 # overrides for pipe_tasks CoaddTask.ConfigClass
 from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
+from lsst.obs.sdss.scaleSdssZeroPoint import ScaleSdssZeroPointTask
 
 root.select.retarget(SelectSdssImagesTask)
+root.scaleZeroPoint.retarget(ScaleSdssZeroPointTask)
