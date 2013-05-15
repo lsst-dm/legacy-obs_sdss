0f954485389ff64ff64455da52fcb5deb829bb81
Yusra AlSayyad <yusra@astro.washington.edu>
2013-05-14 20:19:53 -0500

    processSdssCcd should not run multiShaplet when generating calexps. Removed from default

--- config/processCcd.py
+++ config/processCcd.py
@@ -21,11 +21,3 @@ root.loadSdssWcs = True
 root.calibrate.astrometry.forceKnownWcs = True
 root.calibrate.astrometry.solver.calculateSip = False
 
-try:
-    import lsst.meas.extensions.multiShapelet
-    root.measurement.algorithms.names = set(root.measurement.algorithms.names) | lsst.meas.extensions.multiShapelet.algorithms
-    root.measurement.slots.modelFlux = "multishapelet.combo.flux"
-except ImportError:
-    # TODO: find a better way to log this
-    print "WARNING: Could not import lsst.meas.extensions.multiShapelet; model fluxes not enabled!"
-    
