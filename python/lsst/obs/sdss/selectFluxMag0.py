bb0c0cf82d59b51028382c99539ab1c79cfda441
Yusra AlSayyad <yusra@astro.washington.edu>
2013-06-04 03:02:07 -0500

    Update scaleSdssZeroPoint/selectFluxMag0 to work with changes since #2587 (#2920)

--- python/lsst/obs/sdss/selectFluxMag0.py
+++ python/lsst/obs/sdss/selectFluxMag0.py
@@ -15,76 +15,67 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
-#
+#%
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import MySQLdb
-import os
 
 import lsst.pex.config as pexConfig
 from lsst.afw.coord import IcrsCoord
 import lsst.afw.geom as afwGeom
 from lsst.daf.persistence import DbAuth
 import lsst.pipe.base as pipeBase
-from lsst.pipe.tasks.selectImages import SelectImagesConfig, BaseExposureInfo
+from lsst.pipe.tasks.selectImages import DatabaseSelectImagesConfig, BaseExposureInfo
 
 __all__ = ["SelectSdssFluxMag0Task"]
 
-class SelectSdssFluxMag0Config(SelectImagesConfig):
-    table = pexConfig.Field(
-        doc = "Name of database table",
-        dtype = str,
-        default = "Science_Ccd_Exposure",
-    )
-
+class SelectSdssFluxMag0Config(DatabaseSelectImagesConfig):
+    """Config for SelectSdssFluxMag0Task
+    """
     def setDefaults(self):
-        SelectImagesConfig.setDefaults(self)
+        super(SelectSdssFluxMag0Config,self).setDefaults()
         self.database = "krughoff_early_prod_11032012"
         self.host = "lsst-db.ncsa.illinois.edu"
-        self.port = 3306        
-
+        self.port = 3306
 
 class FluxMagInfo(BaseExposureInfo):
     """Data about a selected exposure
-    
+
     Data includes:
     - dataId: data ID of exposure (a dict)
     - coordList: a list of corner coordinates of the exposure (list of IcrsCoord)
-    - fluxMag0: float 
+    - fluxMag0: float
     - fluxMag0Sigma: float
     """
     def __init__(self, result):
         """Set exposure information based on a query result from a db connection
         """
-        BaseExposureInfo.__init__(self)
-        self.dataId = dict(
-           run = result[self._nextInd],
-           camcol = result[self._nextInd],
-           field = result[self._nextInd],
-           filter = result[self._nextInd],
+        result = [r for r in result]
+        dataId = dict(
+           run = result.pop(0),
+           camcol = result.pop(0),
+           field = result.pop(0),
+           filter = result.pop(0),
         )
-        self.coordList = []
-        for i in range(4):
-            self.coordList.append(
-                IcrsCoord(
-                    afwGeom.Angle(result[self._nextInd], afwGeom.degrees),
-                    afwGeom.Angle(result[self._nextInd], afwGeom.degrees),
-                )
-            )
-        self.fluxMag0 = result[self._nextInd]
-        self.fluxMag0Sigma = result[self._nextInd]
-                
+        coordList = [IcrsCoord(afwGeom.Angle(result.pop(0), afwGeom.degrees),
+                                    afwGeom.Angle(result.pop(0), afwGeom.degrees)) for i in range(4)]
+
+        BaseExposureInfo.__init__(self, dataId=dataId, coordList=coordList)
+        self.fluxMag0 = result.pop(0)
+        self.fluxMag0Sigma = result.pop(0)
+
     @staticmethod
     def getColumnNames():
         """Get database columns to retrieve, in a format useful to the database interface
-        
+
         @return database column names as list of strings
         """
         return (
             "run camcol field  filterName".split() + \
-            "corner1Ra corner1Decl corner2Ra corner2Decl corner3Ra corner3Decl corner4Ra corner4Decl".split() + \
+            "corner1Ra corner1Decl corner2Ra corner2Decl".split() + \
+            "corner3Ra corner3Decl corner4Ra corner4Decl".split() + \
             "fluxMag0 fluxMag0Sigma".split()
         )
 
@@ -92,17 +83,22 @@ class SelectSdssFluxMag0Task(pipeBase.Task):
     """Select SDSS data suitable for computing fluxMag0
     """
     ConfigClass = SelectSdssFluxMag0Config
+    _DefaultName= "selectFluxMag0"
 
     @pipeBase.timeMethod
-    def run(self, coordList, filter, run):
+    def run(self, dataId, coordList):
         """Select flugMag0's of SDSS images for a particular run
 
-        @param[in] filter: filter for images (one of "u", "g", "r", "i" or "z")
+        @param[in] dataId: a dataId containing at least a run and filter
         @param[in] coordList: list of coordinates defining region of interest
-        
+
         @return a pipeBase Struct containing:
         - fluxMagInfoList: a list of FluxMagInfo objects
         """
+        argDict = self.runArgDictFromDataId(dataId)
+        run = argDict["run"]
+        filter = argDict["filter"]
+
         if filter not in set(("u", "g", "r", "i", "z")):
             raise RuntimeError("filter=%r is an invalid name" % (filter,))
 
@@ -111,75 +107,60 @@ class SelectSdssFluxMag0Task(pipeBase.Task):
                       "r": 2,
                       "i": 3,
                       "z": 4}
-                      
-        read_default_file=os.path.expanduser("~/.my.cnf")
 
-        try:
-            open(read_default_file)
-            kwargs = dict(
-                read_default_file=read_default_file,
-                )
-        except IOError:
-            kwargs = dict(
-                user = DbAuth.username(self.config.host, str(self.config.port)),
-                passwd = DbAuth.password(self.config.host, str(self.config.port)),
-                )
-            
-        if self._display:    
-            self.log.info(self.config.table)    
+        if self._display:
             self.log.info(self.config.database)
-        
+
         db = MySQLdb.connect(
             host = self.config.host,
             port = self.config.port,
             db = self.config.database,
-            **kwargs
+            user = DbAuth.username(self.config.host, str(self.config.port)),
+            passwd = DbAuth.password(self.config.host, str(self.config.port)),
         )
         cursor = db.cursor()
-        
+
         columnNames = tuple(FluxMagInfo.getColumnNames())
-        if not columnNames:
-            raise RuntimeError("Bug: no column names")
-       
-        queryStr = "select %s from %s where " % (", ".join(columnNames), self.config.table)
-        dataTuple = () # tuple(columnNames)
+
+        queryStr = "select %s from Science_Ccd_Exposure where " % (", ".join(columnNames))
+        dataTuple = ()
 
         if coordList is not None:
             # look for exposures that overlap the specified region
-            # create table scisql.Region containing patch region
-            coordStrList = ["%s, %s" % (c.getLongitude().asDegrees(),
-                                        c.getLatitude().asDegrees()) for c in coordList]
-            coordStr = ", ".join(coordStrList)
-            queryStr += " scisql_s2PtInCPoly(ra, decl, %s) = 1 and "%(coordStr,)
-       
+            for c in coordList:
+                dataTuple += (c.getLongitude().asDegrees(), c.getLatitude().asDegrees())
+            queryStr += " scisql_s2PtInCPoly(ra, decl"
+            queryStr += ", %s, %s" * len(coordList)
+            queryStr += ") = 1 and "
+
         # compute where clauses as a list of (clause, data)
         whereDataList = [
             ("filterId = %s", filterDict[filter]),
             ("run = %s", run),
         ]
-        
+
         queryStr += " and ".join(wd[0] for wd in whereDataList)
         dataTuple += tuple(wd[1] for wd in whereDataList)
-        
+
         queryStr += " order by field desc"
-        if self._display: 
+        if self._display:
             self.log.info("queryStr=%r; dataTuple=%s" % (queryStr, dataTuple))
-        
+
         cursor.execute(queryStr, dataTuple)
-        exposureInfoList = [FluxMagInfo(result) for result in cursor]        
-        if self._display: 
+        exposureInfoList = [FluxMagInfo(result) for result in cursor]
+        if self._display:
             self.log.info("Found %d exposures" % \
                       (len(exposureInfoList)))
-        
+
         return pipeBase.Struct(
             fluxMagInfoList = exposureInfoList,
         )
 
-    def _runArgDictFromDataId(self, dataId):
+    def runArgDictFromDataId(self, dataId):
         """Extract keyword arguments for run (other than coordList) from a data ID
-        
+
         @param[in] dataId: a data ID dict
-        @return keyword arguments for run (other than coordList), as a dict
+        @return keyword arguments (other than coordList), as a dict
         """
         return dict(
             filter = dataId["filter"],
---
+++ tests/testSelectFluxMag0.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+#
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""Test lsst.obs.sdss.selectFluxMag0Task and integration with lsst.obs.sdss.scaleSdssZeroPointTask
+"""
+import numpy
+import unittest
+
+import lsst.daf.base
+import lsst.afw.geom as afwGeom
+import lsst.afw.image as afwImage
+import lsst.afw.math as afwMath
+import lsst.afw.coord as afwCoord
+
+import lsst.utils.tests as utilsTests
+from lsst.daf.persistence import DbAuth
+from lsst.obs.sdss.scaleSdssZeroPoint import ScaleSdssZeroPointTask
+from lsst.obs.sdss.selectFluxMag0 import SelectSdssFluxMag0Task
+
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+class WrapDataId():
+    """A container for dataId that looks like dataRef to computeImageScaler()
+    """
+    def __init__(self, dataId):
+        self.dataId = dataId
+
+class ScaleSdssZeroPointTaskTestCase(unittest.TestCase):
+    """A test case for ScaleSdssZeroPointTask
+    """
+    def makeTestExposure(self, xNumPix=2060, yNumPix=1967):
+        """
+        Create and return an exposure that is completely covered by the database: test_select_lsst_images
+        """
+        metadata = lsst.daf.base.PropertySet()
+        metadata.set("NAXIS", 2)
+        metadata.set("RADECSYS", "ICRS")
+        metadata.set("EQUINOX", 2000.)
+        metadata.setDouble("CRVAL1", 315.)
+        metadata.setDouble("CRVAL2", 0.)
+        metadata.setDouble("CRPIX1", 68030.)
+        metadata.setDouble("CRPIX2", 30.)
+        metadata.set("CTYPE1", "RA---CEA")
+        metadata.set("CTYPE2", "DEC--CEA")
+        metadata.setDouble("CD1_1", -0.00011)
+        metadata.setDouble("CD1_2", 0.000000)
+        metadata.setDouble("CD2_2", 0.000110)
+        metadata.setDouble("CD2_1", 0.000000)
+        metadata.set("CUNIT1", "deg")
+        metadata.set("CUNIT2", "deg")
+        metadata.set("LTV1", -341970)
+        metadata.set("LTV2", -11412)
+        #exposure needs a wcs and a bbox
+        wcs = afwImage.makeWcs(metadata)
+        bbox = afwGeom.Box2I(afwGeom.Point2I(341970, 11412), afwGeom.Extent2I(xNumPix, yNumPix))
+        exposure = afwImage.ExposureF(bbox, wcs)
+        mi = exposure.getMaskedImage()
+        mi.set(1.0)
+        mi.getVariance().set(1.0)
+        return exposure
+
+    def testSelectFluxMag0(self):
+        """Test SelectFluxMag0"""
+        config = SelectSdssFluxMag0Task.ConfigClass()
+        config.database = "test_select_sdss_images"
+        task = SelectSdssFluxMag0Task(config=config)
+        run = 4192
+        filter = 'i'
+        dataId = {'run': run, "filter": filter}
+        coordList = [afwCoord.Coord(5.62839*afwGeom.radians, -5.66359e-05*afwGeom.radians),
+                     afwCoord.Coord(5.62444*afwGeom.radians, -5.66359e-05*afwGeom.radians),
+                     afwCoord.Coord(5.62444*afwGeom.radians, 0.00371974*afwGeom.radians),
+                     afwCoord.Coord(5.62839*afwGeom.radians, 0.00371974*afwGeom.radians)]
+        fmInfoStruct = task.run(dataId, coordList)
+        fmInfoList = fmInfoStruct.fluxMagInfoList
+        self.assertEqual(2, len(fmInfoList))
+
+
+    def testScaleZeroPoint(self):
+        ZEROPOINT = 27
+        self.sctrl = afwMath.StatisticsControl()
+        self.sctrl.setNanSafe(True)
+
+        config = ScaleSdssZeroPointTask.ConfigClass()
+        config.zeroPoint = ZEROPOINT
+        config.interpStyle = "NATURAL_SPLINE"
+        config.selectFluxMag0.database = "test_select_sdss_images"
+        zpScaler = ScaleSdssZeroPointTask(config=config)
+
+
+        outCalib = zpScaler.getCalib()
+        self.assertAlmostEqual(outCalib.getMagnitude(1.0), ZEROPOINT)
+
+        exposure = self.makeTestExposure()
+        #create dataId for exposure. Visit is only field needed. Others ignored.
+        exposureId = {'ignore_fake_key': 1234, 'run': 4192, 'filter': 'i'}
+
+        #test methods: computeImageScale(), scaleMaskedImage(), getInterpImage()
+        dataRef = WrapDataId(exposureId)
+        imageScaler = zpScaler.computeImageScaler(exposure,dataRef)
+        scaleFactorIm = imageScaler.getInterpImage(exposure.getBBox(afwImage.PARENT))
+
+        predScale = 0.402867736 #image mean for "NATURAL_SPLINE"
+        self.assertAlmostEqual(afwMath.makeStatistics(scaleFactorIm, afwMath.MEAN, self.sctrl).getValue(),
+                               predScale)
+
+        mi = exposure.getMaskedImage()
+        imageScaler.scaleMaskedImage(mi)
+        pixel11 = scaleFactorIm.getArray()[1,1]
+        self.assertAlmostEqual(mi.get(1,1)[0], pixel11) #check image plane scaled
+        self.assertAlmostEqual(mi.get(1,1)[2], pixel11**2) #check variance plane scaled
+
+        exposure.setCalib(zpScaler.getCalib())
+        self.assertAlmostEqual(exposure.getCalib().getFlux(ZEROPOINT), 1.0)
+
+
+    def makeCalib(self, zeroPoint):
+        calib = afwImage.Calib()
+        fluxMag0 = 10**(0.4 * zeroPoint)
+        calib.setFluxMag0(fluxMag0, 1.0)
+        return calib
+
+
+def suite():
+    """Return a suite containing all the test cases in this module.
+    """
+    utilsTests.init()
+
+    suites = [
+        unittest.makeSuite(ScaleSdssZeroPointTaskTestCase),
+        unittest.makeSuite(utilsTests.MemoryTestCase),
+    ]
+
+    return unittest.TestSuite(suites)
+
+
+def run(shouldExit=False):
+    """Run the tests"""
+    config = ScaleSdssZeroPointTask.ConfigClass()
+    try:
+        user = DbAuth.username(config.selectFluxMag0.host, str(config.selectFluxMag0.port)),
+    except Exception, e:
+        print "Warning: did not find host=%s, port=%s in your db-auth file; or %s " \
+              "skipping unit tests" % \
+            (config.selectFluxMag0.host, str(config.selectFluxMag0.port), e)
+        return
+
+    utilsTests.run(suite(), shouldExit)
+
+
+if __name__ == "__main__":
+    run(True)
