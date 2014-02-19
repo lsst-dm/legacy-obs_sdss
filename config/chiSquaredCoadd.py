5aee6c5d767f2c3d9009a9c57879c00d03a38ab2
Russell Owen <rowen@uw.edu>
2014-02-19 12:15:35 -0800

    Remove config overrides for deleted tasks

--- config/chiSquaredCoadd.py
+++
@@ -1,4 +0,0 @@
-# overrides for pipe_tasks ChiSquaredCoaddTask.ConfigClass
-from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
-
-root.select.retarget(SelectSdssImagesTask)
--- config/coadd.py
+++
@@ -1,4 +0,0 @@
-# overrides for pipe_tasks CoaddTask.ConfigClass
-from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
-
-root.select.retarget(SelectSdssImagesTask)
