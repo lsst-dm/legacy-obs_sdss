d204ad2164800139bb1eec89e87a7d593ae533d7
Jim Bosch <jbosch@astro.princeton.edu>
2014-10-09 18:11:38 -0400

    Remove optional dependency on meas_extensions_multiShapelet

--- config/processCoadd.py
+++ config/processCoadd.py
@@ -27,18 +27,3 @@ root.measurement.algorithms['correctfluxes'].apCorrRadius = 18.58 #pixels
 root.detection.thresholdValue = 3.0
 root.doDeblend=True
 root.deblend.maxNumberOfPeaks=40
-
-
-try:
-    # Enable multiShapelet for model mags.
-    import lsst.meas.extensions.multiShapelet
-    root.measurement.algorithms.names |= lsst.meas.extensions.multiShapelet.algorithms
-    root.measurement.slots.modelFlux = "multishapelet.combo.flux"
-    root.measurement.algorithms["multishapelet.psf"].innerOrder = 0
-    root.measurement.algorithms["multishapelet.psf"].outerOrder = 0
-    # too many INTERP pixels on coadds, so we relax the masking in modeling
-    for name in ("exp", "dev", "combo"):
-        root.measurement.algorithms["multishapelet." + name].badMaskPlanes = ["EDGE", "SAT"]
-except ImportError:
-    # TODO: find a better way to log this
-    print "WARNING: Could not import lsst.meas.extensions.multiShapelet; model fluxes not enabled!"
