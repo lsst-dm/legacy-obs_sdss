42447d5876060e3e9f51f43f5d1f2e66cd16ca7a
John K. Parejko <John.Parejko@alumni.carleton.edu>
2015-10-28 16:17:17 -0700

    Add missing license file.

--- python/lsst/obs/sdss/yanny.py
+++ python/lsst/obs/sdss/yanny.py
@@ -1,4 +1,4 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# Licensed under a 3-clause BSD style license - see yanny-LICENSE.rst
 # -*- coding: utf-8 -*-
 """Python library for reading & writing yanny files.
 
