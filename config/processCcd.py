3ec145cb05c38619247ef3dd5808575b4624910f
Russell Owen <rowen@uw.edu>
2016-01-08 17:25:49 -0800

    Update processCcd for new ProcessCcdTask
    Add SdssNullIsrTask and modify the remaining code and config to use it.
    Remove SDSS-specific calibrate task, since we are not using the
    filter-specific configuration and the rest can be handled
    with the new standard CalibrateTask.
    Add new data products to the mapper: icExp, icExpBackground,
    and postISRCCD (the last of which is not written by default, since it is redundant).
    Update ProcessSdssCcdTask accordingly; it is now trivial
    and can be eliminated once DM-4952 is implemented.
    Remove override for ProcessCoaddTask, which is now gone

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,32 +1,46 @@
-from lsst.obs.sdss.calibrate import SdssCalibrateTask
-config.calibrate.retarget(SdssCalibrateTask)
+from lsst.obs.sdss.sdssNullIsr import SdssNullIsrTask
 
-import lsst.meas.astrom.catalogStarSelector
-config.calibrate.u.starSelector.name = "catalog"
-config.calibrate.g.starSelector.name = "catalog"
-config.calibrate.r.starSelector.name = "catalog"
-config.calibrate.i.starSelector.name = "catalog"
-config.calibrate.z.starSelector.name = "catalog"
+# Read post-ISR data from fpC, fpM, asTrans, etc. files and remove overlap
+config.isr.retarget(SdssNullIsrTask)
 
-# Use the PSF determined by SDSS for calibration, without trying to fit anything better,
+# Use the PSF determined by SDSS, without trying to fit anything better,
 # because the exposures cover too small a region to do a good job of PSF fitting
-config.calibrate.useExposurePsf = True
-config.calibrate.doPsf = False
+config.charImage.doMeasurePsf = False
 
-config.calibrate.background.binSize = 512
-config.calibrate.detection.background.binSize = 512
-config.detection.background.binSize = 512
+config.charImage.repair.doInterpolate = False
+config.charImage.repair.doCosmicRay = False
 
-config.loadSdssWcs = True
+config.charImage.background.binSize = 512
+config.charImage.detectAndMeasure.detection.includeThresholdMultiplier = 10.0
+config.charImage.detectAndMeasure.detection.background.binSize = 512
+config.charImage.detectAndMeasure.detection.background.binSize = 512
+config.charImage.detectAndMeasure.measurement.algorithms.names = [
+    "base_SdssCentroid",
+    "base_PixelFlags",
+    "base_SdssShape",
+    "base_PsfFlux",
+    "base_CircularApertureFlux",
+]
+config.charImage.detectAndMeasure.measurement.algorithms["base_CircularApertureFlux"].radii = [7.0]
+config.charImage.detectAndMeasure.measurement.slots.centroid = "base_SdssCentroid"
+config.charImage.detectAndMeasure.measurement.slots.apFlux = "base_CircularApertureFlux_7_0"
+config.charImage.detectAndMeasure.measurement.slots.modelFlux = None
+config.charImage.detectAndMeasure.measurement.slots.instFlux = None
+config.charImage.detectAndMeasure.measurement.slots.calibFlux = "base_CircularApertureFlux_7_0"
+config.charImage.detectAndMeasure.measureApCorr.refFluxName = "slot_CalibFlux"
+
+config.calibrate.detectAndMeasure.detection.background.binSize = 512
+config.calibrate.detectAndMeasure.detection.background.binSize = 512
+# we rarely run PSF determination on SDSS data, so use the output of the star selector instead
+config.calibrate.detectAndMeasure.measureApCorr.inputFilterFlag = "calib_psfCandidate"
+
+# use the WCS determined by SDSS (why?)
 config.calibrate.astrometry.forceKnownWcs = True
 
-#Important for producing coaddTempExps without nans
-config.removeOverlap=False
-
-"""Ap correction defaults to true happens in the measurement algorithm: 'correctFluxes'
-SDSS the standard aperture correction is quoted as out to a radius of 7.43.
-According to http://www.sdss.org/dr7/algorithms/photometry.html#photo_profile this corresponds to 18.58.
-Note that 7.43/0.3961270 = 18.7566 <> 18.58.Why?"""
+# Ap correction defaults to true happens in the measurement algorithm: 'correctFluxes'
+# SDSS the standard aperture correction is quoted as out to a radius of 7.43.
+# According to http://www.sdss.org/dr7/algorithms/photometry.html#photo_profile this corresponds to 18.58.
+# Note that 7.43/0.3961270 = 18.7566 <> 18.58. Why?
 
 #psf flux = ap flux at this radius. Will also be applied to galaxies Same everywhere'
-#config.measurement.algorithms['correctfluxes'].apCorrRadius = 18.58 #pixels
+#config.calibrate.detectAndMeasure.measurement.algorithms['correctfluxes'].apCorrRadius = 18.58 #pixels
--- python/lsst/obs/sdss/processCcdSdss.py
+++ python/lsst/obs/sdss/processCcdSdss.py
@@ -20,41 +20,57 @@
 # the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
-import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
-import lsst.afw.image as afwImage
-import lsst.afw.geom as afwGeom
-from .makeCamera import makeCamera
 from lsst.pipe.tasks.processCcd import ProcessCcdTask
+from .sdssNullIsr import SdssNullIsrTask
 
 class ProcessCcdSdssConfig(ProcessCcdTask.ConfigClass):
     """Config for ProcessCcdSdss"""
-    removePedestal = pexConfig.Field(dtype=bool, default=True, doc="Remove SDSS pedestal from fpC file")
-    pedestalVal = pexConfig.Field(dtype=int, default=1000, doc="Number of counts in the SDSS pedestal")
-
-    removeOverlap = pexConfig.Field(dtype=bool, default=True,
-                                    doc="Remove SDSS field overlap from fpC file")
-    overlapSize = pexConfig.Field(dtype=int, default=128,
-                                  doc="Number of pixels to remove from top of the fpC file")
-    loadSdssWcs = pexConfig.Field(dtype=bool, default=False,
-                                  doc = ("Load WCS from asTrans; it can then be used as-is or updated by "
-                                         "our own code, dependening on calibrate.astrometry parameters.")
-    )
 
     def setDefaults(self):
         ProcessCcdTask.ConfigClass.setDefaults(self)
-        self.doIsr = False
+        self.isr.retarget(SdssNullIsrTask)
+
+## \addtogroup LSST_task_documentation
+## \{
+## \page ProcessCcdSdssTask
+## \ref ProcessCcdSdssTask_ "ProcessCcdSdssTask"
+## \copybrief ProcessCcdSdssTask
+## \}
 
 class ProcessCcdSdssTask(ProcessCcdTask):
-    """Process a CCD for SDSS
+    """!Process SDSS images
+
+    @anchor ProcessCcdSdssTask_
+    
+    @section pipe_tasks_processCcdSdss_Contents  Contents
+
+     - @ref pipe_tasks_processCcdSdss_Purpose
+     - @ref pipe_tasks_processCcdSdss_Initialize
+     - @ref pipe_tasks_processCcdSdss_IO
+     - @ref pipe_tasks_processCcdSdss_Config
+
+    @section pipe_tasks_processCcdSdss_Purpose  Description
+
+    Process SDSS exposures as loaded by SdssNullIsrTask.
+    This trivial subclass of lsst.pipe.tasks.ProcessCcdTask exists solely to provide the correct dataset type
+    to the `--id` command-line argument. Once ticket DM-4952 is implemented this task will not be needed.
+
+    @section pipe_tasks_processCcdSdss_Initialize  Task initialisation
+
+    @copydoc \_\_init\_\_
+
+    @section pipe_tasks_processCcdSdss_IO  Invoking the Task
+
+    The main method is `run`.
+
+    @section pipe_tasks_processCcdSdss_Config  Configuration parameters
+
+    See @ref ProcessCcdSdssConfig
     """
     ConfigClass = ProcessCcdSdssConfig
     _DefaultName = "processCcd"
     dataPrefix = ""
-
-    def __init__(self, **kwargs):
-        ProcessCcdTask.__init__(self, **kwargs)
-        self.camera = makeCamera()
 
     @classmethod
     def _makeArgumentParser(cls):
@@ -62,68 +78,3 @@ class ProcessCcdSdssTask(ProcessCcdTask):
         parser.add_id_argument("--id", datasetType="fpC", help="data ID, e.g. --id run=1 camcol=2 field=345")
         return parser
 
-    @pipeBase.timeMethod
-    def makeExp(self, sensorRef):
-        image = sensorRef.get("fpC").convertF()
-        if self.config.removePedestal:
-            image -= self.config.pedestalVal
-        mask = sensorRef.get("fpM")
-        wcs = sensorRef.get("asTrans")
-        calib, gain = sensorRef.get("tsField")
-        var = afwImage.ImageF(image, True)
-        var /= gain
-
-        mi = afwImage.MaskedImageF(image, mask, var)
-
-        if self.config.removeOverlap:
-            bbox = mi.getBBox(afwImage.LOCAL)
-            begin = bbox.getBegin()
-            extent = bbox.getDimensions()
-            extent -= afwGeom.Extent2I(0, self.config.overlapSize)
-            tbbox = afwGeom.BoxI(begin, extent)
-            mi = afwImage.MaskedImageF(mi, tbbox, True)
-
-        exp = afwImage.ExposureF(mi, wcs)
-        exp.setCalib(calib)
-
-        det = sensorRef.get('camera')["%s%d"%(sensorRef.dataId["filter"], sensorRef.dataId["camcol"])]
-        exp.setDetector(det)
-        exp.setFilter(afwImage.Filter(sensorRef.dataId['filter']))
-
-        # Install the SDSS PSF here; if we want to overwrite it later, we can.
-        psf = sensorRef.get('psField')
-        exp.setPsf(psf)
-
-        return exp
-
-    def setPostIsrExposure(self, sensorRef):
-        """Load the post instrument signature removal image
-
-        \param[in]  sensorRef        sensor-level butler data reference
-
-        \return     postIsrExposure  exposure to be passed to processCcdExposure
-        """
-        if self.config.doCalibrate:
-            exp = self.makeExp(sensorRef)
-            if self.config.loadSdssWcs:
-                self.log.info("Loading WCS from asTrans")
-                wcs = sensorRef.get("asTrans")
-                exp.setWcs(wcs)
-        else:
-            exp = None
-        return exp
-
-    @pipeBase.timeMethod
-    def run(self, sensorRef):
-        """Process a CCD: including source detection, photometry and WCS determination
-
-        @param sensorRef: sensor-level butler data reference to SDSS fpC file
-        @return pipe_base Struct containing these fields:
-        - exposure: calibrated exposure (calexp): as computed if config.doCalibrate,
-            else as upersisted and updated if config.doDetection, else None
-        - calib: object returned by calibration process if config.doCalibrate, else None
-        - sources: detected source if config.doPhotometry, else None
-        """
-        # delegate most of the work to ProcessCcdTask (which, in turn, delegates to ProcessImageTask)
-        result = ProcessCcdTask.run(self, sensorRef)
-        return result
