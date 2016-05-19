7f61c911523cdd156dd06748001fc08059365f31
Russell Owen <rowen@uw.edu>
2016-05-19 14:01:34 -0700

    Fix a str formatting bug in genCameraRegistry

--- bin.src/genCameraRegistry.py
+++ bin.src/genCameraRegistry.py
@@ -5,7 +5,7 @@ import os
 path = lsst.utils.getPackageDir('obs_sdss')
 repoPath = os.path.join(path, 'description', 'camera')
 if os.path.exists(repoPath):
-    raise RuntimeError("Path, $s, exists.  Will not overwrite"%(repoPath))
+    raise RuntimeError("Path %r exists.  Will not overwrite" % (repoPath,))
 else:
     os.makedirs(repoPath)
 
