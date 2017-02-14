fcd3af0d6e71b239b914f40f89a84dba491554ab
Russell Owen <rowen@uw.edu>
2017-02-14 15:00:56 -0800

    Add full `from __future__ import`
    Our pybind11 wrappers require modern division in some places
    and all our code should use:
    ```
    from __future__ import absolute_import, division, print_function
    ```
    in any case. I checked for existing cases where `/` relied on
    returning an integer and found none.
    In at least once instance (image /= float in NullIsrTask)
    this change is required in order to run the code with python 2.

--- python/lsst/obs/sdss/sdssNullIsr.py
+++ python/lsst/obs/sdss/sdssNullIsr.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #
 # LSST Data Management System
 # Copyright 2008-2015 AURA/LSST.
@@ -20,6 +19,7 @@
 # the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
+from __future__ import absolute_import, division, print_function
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 import lsst.afw.image as afwImage
