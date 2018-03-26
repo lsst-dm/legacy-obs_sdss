3a739f7528b9151b7753d531b00607cb370438c6
Simon Krughoff <simon.krughoff@gmail.com>
2018-03-26 13:28:17 -0700

    Remove dependency on MySQLdb

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -1,14 +1,6 @@
 # overrides for pipe_tasks CoaddTask.ConfigClass
-from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
-from lsst.obs.sdss.scaleSdssZeroPoint import ScaleSdssZeroPointTask
-
-config.select.retarget(SelectSdssImagesTask)
-config.scaleZeroPoint.retarget(ScaleSdssZeroPointTask)
-
 config.subregionSize = (2500, 2500)
 config.sigmaClip = 5
 
 # Configs for deep coadd
 config.coaddName = 'deep'
-config.select.maxFwhm = 2.0
-config.select.quality = 2
--- config/chiSquaredCoadd.py
+++
@@ -1,4 +0,0 @@
-# overrides for pipe_tasks ChiSquaredCoaddTask.ConfigClass
-from lsst.obs.sdss.selectSdssImages import SelectSdssImagesTask
-
-config.select.retarget(SelectSdssImagesTask)
